b27a9377dba94c049a1f9263e5a7f760117a5d83
Splintex <eugene.godin@gmail.com>
2014-02-21 14:23:57 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 
+	$(".js-slider").on('cycle-before', function() {
+		$(this).find(".is-inactive").removeClass("is-inactive");
+		$(this).find(".is-active").addClass("is-inactive");
+	});
+	$(".js-place-slider .cycle-slide-active").next().next().addClass("is-current");
+	$(".js-place-slider").on('cycle-next', function() {
+		$(this).find(".is-current").removeClass("is-current");
+		$(this).find(".cycle-slide-active").next().next().next().addClass("is-current");
+		$(this).find(".is-active").addClass("is-inactive");
+	});
+	$(".js-place-slider").on('cycle-prev', function() {
+		$(this).find(".is-current").removeClass("is-current");
+		$(this).find(".cycle-slide-active").next().addClass("is-current");
+		$(this).find(".is-active").addClass("is-inactive");
+	});
+
+
+
 });

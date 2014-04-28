f7b1aaa6f7f247b7c34be2c86effa6bcd9b1e7f6
Splintex <eugene.godin@gmail.com>
2014-04-28 12:15:22 +0300

    upd

--- mobile/js/init.js
+++ mobile/js/init.js
@@ -1,1 +1,6 @@
-head.load("js/lib/jquery.js","http://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=ru-RU", "js/common.js");
+head.load("js/lib/jquery.js",
+	"js/lib/fotorama.js",
+	"http://api-maps.yandex.ru/2.0-stable/?load=package.standard&lang=ru-RU", 
+	"js/common.js"
+);
+  
